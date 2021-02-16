df7f38a26a77e60fe6dd5ba4befccefdba153117
David Jun <djun301@gmail.com>
2021-02-16 03:29:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
 data.each_with_object({}) do |(key, value), final_array|
 value.each do |in_key, name|
 name.each do |name|
+  binding.pry
 if !final_array[name]
   final_array[name] = {}
 end
